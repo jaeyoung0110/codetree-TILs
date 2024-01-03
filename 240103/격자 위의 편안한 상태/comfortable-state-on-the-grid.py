bbbfa404addb95f5b610d408c44d87c6209e1baa
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-01-03 19:06:34 +0900

    Title: 격자 위의 편안한 상태, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240103/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,25 @@
+dx = [1,0,-1,0]
+dy = [0,-1,0,1]
+
+n, m = map(int,input().split())
+
+coo = [[0 for i in range(n)] for i in range(n)]
+
+def isPA(x,y) :
+    cnt = 0
+    for i in range(4) :
+        try :
+            if coo[x+dx[i]][y+dy[i]] == 1 :
+                cnt += 1
+        except :
+            continue
+    if cnt == 3 :
+        return 1
+    else :
+        return 0
+
+
+for i in range(m) :
+    r, c = map(int,input().split())
+    coo[r-1][c-1] = 1
+    print(isPA(r-1,c-1))
