c327bcf7966185d4992fc99a493b295dc1e8315b
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-01-06 20:28:23 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 254ms, Memory: 29MB, Status: Passed - CodeTree

--- 240106/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240106/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -6,7 +6,7 @@ data_coo = []
 for i in range(N) :
     data_coo.append(list(map(int,input().split())))
 
-min_dis = 99999
+min_dis = 0
 
 for j in range(1,N-1) :
     copy_coo = copy.deepcopy(data_coo)
@@ -14,6 +14,9 @@ for j in range(1,N-1) :
     dis = 0
     for i in range(N-2) :
         dis += (abs(copy_coo[i+1][0]-copy_coo[i][0])+abs(copy_coo[i+1][1]-copy_coo[i][1]))
+    if j == 1 :
+        min_dis = dis
+        continue
     min_dis = min(min_dis,dis)
 
 print(min_dis)
