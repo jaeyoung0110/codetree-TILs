5953eb0932266fb170ccc451fad270d938e74761
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-01-07 11:33:42 +0900

    Title: 최고의 13위치 2, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240107/최고의 13위치 2/best-place-of-13-2.py
+++ 240107/최고의 13위치 2/best-place-of-13-2.py
@@ -10,7 +10,7 @@ for i in range(N) :
         for ii in range(N) :
             for jj in range(N-2) :
                 # i,j / ii,jj / 겹치면 패스
-                if ii == i and (jj == j or jj == j+1 or jj == j+2 or jj == j-1 or jj == j-2) :
+                if ii == i and abs(jj-j) <= 2 :
                     continue
                 # 개수 최대 판단
                 max_cnt = max(max_cnt, coo[i][j]+coo[i][j+1]+coo[i][j+2]+coo[ii][jj]+coo[ii][jj+1]+coo[ii][jj+2])
