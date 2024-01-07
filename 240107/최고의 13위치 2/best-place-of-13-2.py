9f2865f0ca8dae71c5d4cf2386dc139050b0964a
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-01-07 11:30:20 +0900

    Title: 최고의 13위치 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240107/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,18 @@
+N = int(input())
+coo = []
+for _ in range(N) :
+    coo.append(list(map(int,input().split())))
+
+max_cnt = 0
+
+for i in range(N) :
+    for j in range(N-2) :
+        for ii in range(N) :
+            for jj in range(N-2) :
+                # i,j / ii,jj / 겹치면 패스
+                if ii == i and (jj == j or jj == j+1 or jj == j+2) :
+                    continue
+                # 개수 최대 판단
+                max_cnt = max(max_cnt, coo[i][j]+coo[i][j+1]+coo[i][j+2]+coo[ii][jj]+coo[ii][jj+1]+coo[ii][jj+2])
+
+print(max_cnt)
