2a577b78cb8346d9be37c817abd8ee84e39dc99d
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-01-08 14:31:18 +0900

    Title: G or H 2, Time: 164ms, Memory: 26MB, Status: Passed - CodeTree

--- 240108/G or H 2/G-or-H-2.py
+++ 240108/G or H 2/G-or-H-2.py
@@ -20,7 +20,7 @@ for i in range(101) : # i = 사진 시작 위치
                 cnt_G += 1
                 if cnt_G == 1 :
                     idx_G[0] = k
-                    idx_G[0] = k
+                    idx_G[1] = k
                 else :
                     idx_G[1] = k
             elif coo[k] == 'H' : 
@@ -33,10 +33,13 @@ for i in range(101) : # i = 사진 시작 위치
         # G H 값 동일
         if cnt_G == cnt_H and cnt_G > 0 and cnt_H > 0 :
             max_len = max(max_len, max(idx_G[1],idx_H[1])-min(idx_G[0],idx_H[0]))
+        # G 만
         elif cnt_G > 0 and cnt_H == 0 :
             max_len = max(max_len, idx_G[1] - idx_G[0])
+        # H 만
         elif cnt_G == 0 and cnt_H > 0 :
             max_len = max(max_len, idx_H[1] - idx_H[0])
+
 
 
 print(max_len)
