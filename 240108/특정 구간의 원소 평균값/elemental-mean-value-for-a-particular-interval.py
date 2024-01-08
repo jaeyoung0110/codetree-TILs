58d28bd7ed8c833dc40831b4acfd8bcf77e30de9
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-01-08 11:38:48 +0900

    Title: 특정 구간의 원소 평균값, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240108/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240108/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -5,13 +5,18 @@ ans_cnt = 0
 
 for i in range(N) :
     for j in range(i, N) :
+        # 구간 크기 0 이면 그냥 맞음
         if i == j :
             ans_cnt += 1
             continue
+
+        # 평균 구하기
         mean_cnt = 0
         for k in range(i,j+1) :
             mean_cnt += arr[k]
-        mean_cnt /= j-i
+        mean_cnt /= j-i+1
+
+        # 평균과 같은 원소 있는지 검증
         for k in range(i,j+1) :
             if mean_cnt == arr[k] :
                 ans_cnt += 1
