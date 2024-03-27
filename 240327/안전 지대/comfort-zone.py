a48ba2cb12d5eb667a35073fbe718512d14074e6
jojaeyoung <70993185+jaeyoung0110@users.noreply.github.com>
2024-03-27 21:06:26 +0900

    Title: 안전 지대, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/안전 지대/comfort-zone.py
@@ -0,0 +1,37 @@
+## k값에 따라 그리드DFS 해서 덩어리 개수 구함
+## 최댓값 갱신 + 최댓값에 k도 같이 저장
+ans = [0,0]
+
+N, M = map(int,input().split())
+grid = []
+for _ in range(N) :
+    grid.append(list(map(int,input().split())))
+
+def is_range(r,c) :
+    if r >= 0 and r < N and c >= 0 and c < M :
+        return True
+    return False
+
+def dfs(vertex_r,vertex_c, visited, k) :
+    mr = [-1,0,1,0]
+    mc = [0,1,0,-1]
+    visited[vertex_r][vertex_c] = True
+    for dr, dc in zip(mr,mc) : 
+        if is_range(vertex_r+dr,vertex_c+dc) and grid[vertex_r+dr][vertex_c+dc] > k and not visited[vertex_r+dr][vertex_c+dc] :
+            visited[vertex_r+dr][vertex_c+dc] = True
+            dfs(vertex_r+dr,vertex_c+dc,visited,k)
+
+
+for k in range(1,101) :
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    cnt_city = 0
+    for r in range(N) :
+        for c in range(M) :
+            if grid[r][c] > k and not visited[r][c]:
+                cnt_city += 1
+                dfs(r,c,visited,k)
+    if ans[0] < cnt_city :
+        ans[0] = cnt_city
+        ans[1] = k
+
+print(ans[1],ans[0])
